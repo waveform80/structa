b276d2c6899603ed9d369a271cfdd95a7505e1d8
Dave Jones <dave@waveform.org.uk>
2020-11-20 01:31:18 +0000

    Add humanize and blessings for nicer output
    Implement a nice progress bar, ETA display, etc.
    
    Closes: #6

--- structa/ui.py
+++ structa/ui.py
@@ -9,7 +9,10 @@ from datetime import datetime, timedelta
 from fractions import Fraction
 from threading import Thread
 from queue import Queue
+from time import sleep
 
+import humanize
+from blessings import Terminal
 from chardet.universaldetector import UniversalDetector
 
 from . import analyzer, patterns
@@ -20,30 +23,30 @@ def main(args=None):
     warnings.simplefilter('ignore', category=analyzer.ValidationWarning)
     try:
         config = get_config(args)
-        a = Analyzer.from_config(config)
-        d = load_data(config)
-        q = Queue()
-        thread = Thread(
-            target=lambda a, d, q: q.put(a.analyze(d)),
-            args=(a, d, q),
-            daemon=True
-        )
-        thread.start()
-        while True:
-            if a.progress is None:
-                print('\rMeasuring size',
-                      end='', file=sys.stderr, flush=True)
-            else:
-                print('\rAnalyzing: {p:.1f}%  '
-                      .format(p=100 * float(a.progress)),
-                      end='', file=sys.stderr, flush=True)
-            thread.join(0.1)
-            if not thread.is_alive():
-                break
-        print()
+        with Progress() as progress:
+            a = Analyzer.from_config(config)
+            d = load_data(config, progress)
+            q = Queue()
+            thread = Thread(
+                target=lambda a, d, q: q.put(a.analyze(d)),
+                args=(a, d, q),
+                daemon=True
+            )
+            thread.start()
+            if a.track_progress:
+                progress.message('Measuring size')
+            analyzing = False
+            while True:
+                if a.progress is not None:
+                    if not analyzing:
+                        analyzing = True
+                        progress.message('Analyzing structure')
+                    progress.position = a.progress
+                thread.join(0.25)
+                if not thread.is_alive():
+                    break
         print(q.get(timeout=1, block=True))
     except Exception as e:
-        print('', file=sys.stderr)
         debug = int(os.environ.get('DEBUG', '0'))
         if not debug:
             print(str(e), file=sys.stderr)
@@ -167,6 +170,88 @@ class Analyzer(analyzer.Analyzer):
             track_progress=True)
 
 
+class Progress:
+    def __init__(self, output=sys.stderr, show_bar=True, show_percent=True,
+                 show_eta=True, show_spinner=False):
+        self.term = Terminal()
+        self.output = output
+        self._show_bar = show_bar
+        self._show_percent = show_percent
+        self._show_eta = show_eta
+        self._show_spinner = show_spinner
+        self._started = None
+        self._position = 0.0
+        self._last_spinner = '-'
+
+    def hide(self):
+        self.output.write(self.term.clear_bol + self.term.move_x(0))
+
+    def show(self):
+        eta = pct = bar = spin = ''
+        if self._show_eta and self._started is not None and self._position > 0.1:
+            eta = ' ETA {eta} '.format(
+                eta=humanize.naturaldelta(
+                    (1 - self._position) * (datetime.now() - self._started)
+                    / self._position))
+        if self._show_percent:
+            pct = ' {p:4.1f}% '.format(p=self._position * 100)
+        if self._show_spinner:
+            self._last_spinner = {
+                '|': '/',
+                '/': '-',
+                '-': '\\',
+                '\\': '|',
+            }[self._last_spinner]
+            spin = ' ' + self._last_spinner
+        if self._show_bar:
+            size = self.term.width - len(pct) - len(eta) - len(spin) - 3
+            bar = '[{bar:.<{size}}]'.format(
+                size=size, bar='#' * int(size * self._position))
+        self.output.write(self.term.move_x(0))
+        self.output.write(self.term.black_on_green)
+        self.output.write(pct)
+        self.output.write(eta)
+        self.output.write(self.term.normal)
+        self.output.write(bar)
+        self.output.write(spin)
+        self.output.flush()
+
+    def message(self, msg):
+        """
+        Output the message *s* to the :attr:`output`.
+        """
+        self.hide()
+        self.output.write(msg)
+        self.output.write('\n')
+        self.show()
+
+    @property
+    def position(self):
+        """
+        The progress of the activity; a floating-point value between 0.0
+        (just begun) and 1.0 (finished). Set this property to update the
+        progress bar.
+        """
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        if self._started is None:
+            self._started = datetime.now()
+        value = min(1.0, max(0.0, value))
+        self._position = float(value)
+        self.show()
+
+    def __enter__(self):
+        self._position = 0.0
+        self._started = None
+        return self
+
+    def __exit__(self, *exc):
+        self.hide()
+        self.output.flush()
+
+
 def detect_encoding(config):
     # XXX Split this into a separate AutoEncodedFile class? Could probably
     # make the sampling a bit nicer (e.g. hide it behind a "seekable" file
@@ -211,9 +296,8 @@ def detect_format(config):
                 config.format = 'csv'
                 if config.csv_format == 'auto':
                     # First line is possible header, last is possibly partial
-                    sample = sample[1:-1]
                     config.csv_dialect = csv.Sniffer().sniff(
-                        sample, delimiters=",; \t")
+                        sample[:1024], delimiters=",; \t")
                 else:
                     class dialect(csv.Dialect):
                         delimiter = config.csv_format[:1]
@@ -225,27 +309,22 @@ def detect_format(config):
                         lineterminator = '\r\n'
                         quoting = csv.QUOTE_MINIMAL
                     config.csv_dialect = dialect
-        print('Detected format {format}'.format(format=config.format.upper()),
-              file=sys.stderr)
-        # XXX Output CSV detected dialect?
+                # XXX Output CSV detected dialect?
 
 
-def load_data(config):
+def load_data(config, progress):
     detect_encoding(config)
-    print('Detected encoding {config.encoding}'.format(config=config),
-          file=sys.stderr, flush=True)
+    progress.message('Detected encoding {config.encoding}'.format(config=config))
     detect_format(config)
-    print('Detected format {config.format}'.format(config=config),
-          file=sys.stderr, flush=True)
-    print('Reading file {config.file.name}'.format(config=config),
-          file=sys.stderr, flush=True)
+    progress.message('Detected format {config.format}'.format(config=config))
+    progress.message('Reading file {config.file.name}'.format(config=config))
     data = config.sample + config.file.read()
-    print('Decoding file', file=sys.stderr, flush=True)
+    progress.message('Decoding file')
     data = data.decode(
         config.encoding,
         errors='strict' if config.encoding_strict else 'replace')
 
-    print('Parsing data', file=sys.stderr, flush=True)
+    progress.message('Parsing data')
     if config.format == 'json':
         return json.loads(data)
     elif config.format == 'csv':
